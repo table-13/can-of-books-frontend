04602c4297ed6ddbcfd085fe5535a797ee60fc57
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-09-28 16:18:30 -0700

    made create book form

--- src/components/BestBooks.js
+++ src/components/BestBooks.js
@@ -2,8 +2,12 @@ import axios from "axios";
 import { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Carousel from "react-bootstrap/Carousel";
+import Button from "react-bootstrap/Button"
+import CreateForm from "./CreateForm.js"
 
+//let server = process.env.REACT_APP_API_URL;  
 export default class BestBooks extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {
@@ -14,15 +18,17 @@ export default class BestBooks extends Component {
 
   }
 
+  handleBookCreate = async (bookInfo) => {
+    const bookResponse = await axios.post(`http://localhost:3009/books`, bookInfo);
+    this.fetchBooks();
+  }
   componentDidMount() {
     this.fetchBooks();
   }
 
 async fetchBooks() {
-  let apiURL = 'http://localhost:3009/books';
-
   try {
-    const response = await axios.get (apiURL);
+    const response = await axios.get (`http://localhost:3009/books`);
     const books = response.data;
     console.log(books);
     if(books.length > 0) {
@@ -33,9 +39,6 @@ async fetchBooks() {
     console.log('we broke');
   }
 }
-
-
-
   render() {
     return (
       <Container fluid>
@@ -50,9 +53,9 @@ async fetchBooks() {
         </Carousel.Item>
         )}
         </Carousel>
-
+          {/* <Button onClick={this.state.createBook} /> */}
         {this.state.emptyMessage && <h1>{this.state.emptyMessage}</h1> }
-
+        <CreateForm createBook={this.handleBookCreate}/>
       </Container>
     );
   }
