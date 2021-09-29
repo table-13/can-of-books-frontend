d962356715761f3cd8a1746b1c5f22e0adc05a5a
Tripp Lucas <tripppdx@gmail.com>
2021-09-29 14:05:15 -0700

    move nav to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
-//import { Link } from "react-router-dom";
+import { Navbar, NavItem } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "../Header.css";
 
 class Header extends React.Component {
   render() {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>My Favorite Books</Navbar.Brand>
-        {/* <NavItem><Link to="/" className="nav-link">Home</Link></NavItem> */}
+        <Navbar.Brand>Can of Books</Navbar.Brand>
+        <NavItem>
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/Profile" className="nav-link">
+            Profile
+          </Link>
+        </NavItem>
         {/* TODO: if the user is logged in, render a navigation link to profile page */}
         {/* TODO: if the user is logged in, render the `LogoutButton` */}
       </Navbar>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,5 @@
-
 import { Component } from "react";
-// import BestBooks from ".BestBooks.js";
-
-
 import BestBooks from "./BestBooks";
-import Profile from "./Profile";
-
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from "react-router-dom";
 
 export default class Main extends Component {
   constructor(props) {
@@ -23,29 +10,9 @@ export default class Main extends Component {
   }
   render() {
     return (
-      
       <>
-      <Router>
-        <nav>
-          <h1>Can of Books</h1>
-          <Link to="/BestBooks">Home</Link>
-          <Link to="/Profile">Profile</Link>
-        </nav>
-        <Switch>
-          <Route path="/BestBooks">
-          <BestBooks />
-          </Route>
-          <Route path="/Profile">
-           <Profile />
-          </Route>
-        </Switch>
-      </Router>
-
-      
-     
-    </>
-        
-        
+        <BestBooks />
+      </>
     );
   }
 }
