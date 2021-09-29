1e05c423ae5d06a89826a3a095f6e6f98aa17618
Tripp Lucas <tripppdx@gmail.com>
2021-09-29 00:22:39 -0700

    Netflify build failure fix 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Navbar, NavItem } from 'react-bootstrap';
+import { Navbar } from "react-bootstrap";
 //import { Link } from "react-router-dom";
 import "../Header.css";
 
